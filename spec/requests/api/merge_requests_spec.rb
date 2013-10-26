5d6e97fbe4c4ef4600b872a749a1c495109e0a6c
Finn Arne Gangstad <finnag@gmail.com>
2013-10-26 21:57:13 +0200

    expose description in MergeRequest API

--- spec/requests/api/merge_requests_spec.rb
+++ spec/requests/api/merge_requests_spec.rb
@@ -6,7 +6,7 @@ describe API::API do
   after(:each) { ActiveRecord::Base.observers.disable(:user_observer) }
   let(:user) { create(:user) }
   let!(:project) {create(:project_with_code, creator_id: user.id, namespace: user.namespace) }
-  let!(:merge_request) { create(:merge_request, author: user, assignee: user, source_project: project, target_project: project, title: "Test") }
+  let!(:merge_request) { create(:merge_request, author: user, assignee: user, source_project: project, target_project: project, title: "Test", description: "Test description") }
   before {
     project.team << [user, :reporters]
   }
@@ -25,6 +25,7 @@ describe API::API do
         response.status.should == 200
         json_response.should be_an Array
         json_response.first['title'].should == merge_request.title
+        json_response.first['description'].should == merge_request.description
       end
     end
   end
@@ -47,9 +48,10 @@ describe API::API do
     context 'between branches projects' do
       it "should return merge_request" do
         post api("/projects/#{project.id}/merge_requests", user),
-             title: 'Test merge_request', source_branch: "stable", target_branch: "master", author: user
+             title: 'Test merge_request', source_branch: "stable", target_branch: "master", author: user, description: "Test desc"
         response.status.should == 201
         json_response['title'].should == 'Test merge_request'
+        json_response['description'].should == 'Test desc'
       end
 
       it "should return 422 when source_branch equals target_branch" do
